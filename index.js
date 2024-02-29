4a6b8208ea97444809bd406e4f6ebc6c15335757
DIYA GOYAL <122631622+DiyaGoyal0212@users.noreply.github.com>
2024-02-29 11:41:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,191 @@
+let searchBox = document.querySelector('.searchBox');
+let  searchbtn = document.querySelector('.searchbtn');
+ let anchor = document.querySelector('.anchor');
+
+
+ 
+
+ searchbtn.addEventListener('click',(e)=>{
+     e.preventDefault();
+     const searchinput= searchBox.value.trim();
+ 
+    anchor.href=`.${searchinput}`;
+     
+ })
+ var blur = document.getElementById('blur');
+ var nav = document.querySelector('nav');
+
+ function toggle(){
+   
+     blur.classList.toggle('active');
+     nav.classList.toggle('active');
+     var popup = document.getElementById('popup');
+     popup.classList.toggle('active');
+     
+ }
+ function toggl(){
+     
+     blur.classList.toggle('active');
+     nav.classList.toggle('active');
+     var popup2 = document.getElementById('popup2');
+     popup2.classList.toggle('active');
+     
+ }
+ function tiramisu(){
+     blur.classList.toggle('active');
+     nav.classList.toggle('active');
+     var popup3 = document.getElementById('popup3');
+     popup3.classList.toggle('active');
+ }
+ function cheese(){
+     blur.classList.toggle('active');
+     nav.classList.toggle('active');
+     var popup4 = document.getElementById('popup4');
+     popup4.classList.toggle('active');
+
+ }
+ function burfi(){
+     
+     blur.classList.toggle('active');
+     nav.classList.toggle('active');
+     var popup5 = document.getElementById('popup5');
+     popup5.classList.toggle('active');
+     
+ }
+ function rasmalai(){
+     
+     blur.classList.toggle('active');
+     nav.classList.toggle('active');
+     var popup6 = document.getElementById('popup6');
+     popup6.classList.toggle('active');
+     
+ }
+ function rasgulla(){
+     
+     blur.classList.toggle('active');
+     nav.classList.toggle('active');
+     var popup7 = document.getElementById('popup7');
+     popup7.classList.toggle('active');
+     
+ }
+ function gulab(){
+     blur.classList.toggle('active');
+     nav.classList.toggle('active');
+     var popup8 = document.getElementById('popup8');
+     popup8.classList.toggle('active');
+
+ }
+ function pasta(){
+     blur.classList.toggle('active');
+     nav.classList.toggle('active');
+     var popup9 = document.getElementById('popup9');
+     popup9.classList.toggle('active');
+
+ }
+ 
+ function pizza(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup10 = document.getElementById('popup10');
+    popup10.classList.toggle('active');
+
+}
+function sandwich(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup11 = document.getElementById('popup11');
+    popup11.classList.toggle('active');
+
+}
+function burgir(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup12 = document.getElementById('popup12');
+    popup12.classList.toggle('active');
+
+}
+function gobhi(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup13= document.getElementById('popup13');
+    popup13.classList.toggle('active');
+
+}
+function bhindi(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup14= document.getElementById('popup14');
+    popup14.classList.toggle('active');
+
+}
+function makhani(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup15= document.getElementById('popup15');
+    popup15.classList.toggle('active');
+
+}
+function tofu(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup16= document.getElementById('popup16');
+    popup16.classList.toggle('active');
+
+}
+function mushroom(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup17= document.getElementById('popup17');
+    popup17.classList.toggle('active');
+
+}
+function kadhi(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup18= document.getElementById('popup18');
+    popup18.classList.toggle('active');
+
+}
+function rajma(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup19= document.getElementById('popup19');
+    popup19.classList.toggle('active');
+
+}
+function lababdar(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup20= document.getElementById('popup20');
+    popup20.classList.toggle('active');
+
+}
+function toast(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup21= document.getElementById('popup21');
+    popup21.classList.toggle('active');
+
+}
+function poha(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup22= document.getElementById('popup22');
+    popup22.classList.toggle('active');
+
+}
+function idli(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup23= document.getElementById('popup23');
+    popup23.classList.toggle('active');
+
+}
+function dosa(){
+    blur.classList.toggle('active');
+    nav.classList.toggle('active');
+    var popup24= document.getElementById('popup24');
+    popup24.classList.toggle('active');
+
+}
+
